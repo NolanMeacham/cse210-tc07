f4d68135645e94c43774f55257a6672af7bf0660
Nick Velasquez <nvelasquez596@gmail.com>
2021-02-19 14:44:48 -0700

    added the rest of the Posistion functions

--- speed/game/position.py
+++ speed/game/position.py
@@ -31,7 +31,7 @@ class Position():
         Gets the horizontal distance.
         
         Args:
-            self (Point): An instance of Point.
+            self (Position): An instance of Position.
             
         Returns:
             integer: The horizontal distance.
@@ -43,9 +43,48 @@ class Position():
         Gets the vertical distance.
         
         Args:
-            self (Point): An instance of Point.
+            self (Position): An instance of Position.
             
         Returns:
             integer: The vertical distance.
         """
         return self._y
+    
+    def add(self, other):
+        """Gets a new Position that is the sum of this and the given one.
+
+        Args:
+            self (Position): An instance of Position.
+            other (Position): The Position to add.
+
+        Returns:
+            Position: A new Position that is the sum.
+        """
+        x = self._x + other.get_x()
+        y = self._y + other.get_y()
+        return Position(x, y)
+
+    def equals(self, other):
+        """Whether or not this Position is equal to the given one.
+
+        Args:
+            self (Position): An instance of Position.
+            other (Position): The Position to compare.
+
+        Returns: 
+            boolean: True if both x and y are equal; false if otherwise.
+        """
+        return self._x == other.get_x() and self._y == other.get_y()
+    
+    def reverse(self):
+        """Gets a new Position that is the reverse of this one.
+        
+        Args:
+            self (Position): An instance of Position.
+            
+        Returns:
+            Position: A new Position that is reversed.
+        """
+        x = self._x * -1
+        y = self._y * -1
+        return Position(x, y)
