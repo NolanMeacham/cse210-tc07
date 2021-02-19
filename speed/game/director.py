f34773312fff625b1410ca4a53e537891925a95e
Nolan Meacham <nolan.meacham@gmail.com>
2021-02-19 13:40:43 -0700

    updates for start_game method

--- speed/game/director.py
+++ speed/game/director.py
@@ -20,24 +20,33 @@ class Director:
         self._input_service = input_service
         self._output_service = output_service
 
-        self.keep_playing = True
+        self._keep_playing = True
 
     def start_game(self):
         """
+
         """
-        pass
+        
+        while self._keep_playing:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+            sleep(constants.FRAME_LENGTH)
 
     def _get_inputs(self):
         """
+
         """
         pass
 
     def _do_updates(self):
         """
+
         """
         pass
 
     def _do_outputs(self):
         """
+        
         """
         pass
