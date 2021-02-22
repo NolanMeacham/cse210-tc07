d194d8b2787c713b7c2340a61e9c68ea4795faac
Nick Velasquez <nvelasquez596@gmail.com>
2021-02-22 13:42:05 -0700

    buffers should work now

--- speed/game/buffer.py
+++ speed/game/buffer.py
@@ -2,6 +2,7 @@
 # import statements here
 from game.actor import Actor
 from game.position import Position
+import numpy as np
 
 class Buffer(Actor):
     """
@@ -15,26 +16,39 @@ class Buffer(Actor):
         """
         super().__init__()
         self._buffer = []        
-        self._text_buffer = '-Buffer:{}'
-    
-    def init_buffer(self):
+        self._display_buffer = '-Buffer:'
+        self._text_buffer = ''
+
+    def reset_buffer(self):
+        self._buffer = []
         for i in range(52):
             self._buffer.append('-')
 
-    def clear_buffer(self):
-        pass
-
-    def set_letter(self):
-        pass
+    def set_letter(self, letter):
+        self._buffer.append(letter)
+        self._buffer = np.roll(self._buffer,-1)
+        self._buffer = np.resize(self._buffer, self._buffer.size - 1)
+        self._buffer= self._buffer.tolist()
 
     def get_buffer(self):
         return self._buffer
 
-    def get_buffer_display(self):
-        pass 
+    def set_display_buffer(self):
+        self._display_buffer = '-Buffer:'
+        for i in self._buffer:
+            self._display_buffer += i
+    
+    def get_display_buffer(self):
+        return self._display_buffer
+    
+    def set_text_buffer(self):
+        self._text_buffer = ''
+        for i in self._buffer:
+            if i == '-':
+                pass
+            else:
+                self._text_buffer += i
+    
+    def get_text_buffer(self):
+        return self._text_buffer
 
-# test = Buffer()
-
-# test.init_buffer()
-
-# print(test.get_buffer())
