8d644d1b1ee4b0a5c191e38e33d849b2faf8afe1
Nick Velasquez <nvelasquez596@gmail.com>
2021-02-22 10:30:08 -0700

    adding buffer to main

--- speed/game/buffer.py
+++ speed/game/buffer.py
@@ -1,3 +1,4 @@
+
 # import statements here
 from game.actor import Actor
 from game.position import Position
@@ -13,4 +14,27 @@ class Buffer(Actor):
 
         """
         super.__init__()
+        self._buffer = []        
+        self._text_buffer = '-Buffer:{}'
+    
+    def init_buffer(self):
+        for i in range(52):
+            self._buffer.append('-')
 
+    def clear_buffer(self):
+        pass
+
+    def set_letter(self):
+        pass
+
+    def get_buffer(self):
+        return self._buffer
+
+    def get_buffer_display(self):
+        pass 
+
+test = Buffer()
+
+test.init_buffer()
+
+print(test.get_buffer())
